fc7e6994c9012be84c81a0c82cb3f64a99fd450e
MatsLanGoH <mats.lan.pod@googlemail.com>
2017-07-24 15:44:42 +0900

    Add single hotel to MongoDB

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -46,10 +46,31 @@ module.exports.getSingleHotel = function (req, res) {
 
 };
 
+
 module.exports.addSingleHotel = function (req, res) {
+  // Connect to database
+  var db = dbconn.get();
+  var collection = db.collection('hotels');
+  var newHotel;
+
   console.log("POST new hotel");
-  console.log(req.body);
-  res
-    .status(200)
-    .json(req.body);
+  if (req.body && req.body.name && req.body.stars) {
+
+    newHotel = req.body;
+    newHotel.stars = parseInt(req.body.stars, 10);
+
+    console.log(newHotel);
+    collection.insertOne(newHotel, function(err, response) {
+      console.log(response);
+      res
+        .status(201)
+        .json(response.ops);
+    })
+
+  } else {
+    console.log("Data missing from POST request");
+    res
+      .status(400)
+      .json({message : "Required data missing from body"});
+  }
 }
