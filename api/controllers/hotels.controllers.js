deb7f3b234912e397c6c70eadae88f1c3032a805
MatsLanGoH <mats.lan.pod@googlemail.com>
2017-07-25 17:47:24 +0900

    Replaced query controllers with mongoose calls.

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,50 +1,50 @@
-var dbconn = require('../data/dbconnection.js');
-var ObjectId = require('mongodb').ObjectId;
+// var dbconn = require('../data/dbconnection.js');
+// var ObjectId = require('mongodb').ObjectId;
+var mongoose = require('mongoose');
+var Hotel = mongoose.model('Hotel');
 
+// Returns json for all hotels in db
 module.exports.getAllHotels = function (req, res) { // Controller
-
-  // Connect to database
-  var db = dbconn.get();
-  var collection = db.collection('hotels');
 
   // Get values for pagination from query (or use default if none given)
   var offset = parseInt(req.query.offset) || 0;
   var count = parseInt(req.query.count) || 5;
 
-  collection
+  // Query model
+  Hotel
     .find()
     .skip(offset)
     .limit(count)
-    .toArray(function(err, docs) {
-      console.log("Found hotels", docs);
-      res
-        .status(200)
-        .json(docs);
+    .exec(function(err, hotels) {
+      console.log(hotels);
+      res.json(hotels);
     });
-
 };
 
+// Returns json for a single hotel.
 module.exports.getSingleHotel = function (req, res) {
 
-  // Connect to database
-  var db = dbconn.get();
-  var collection = db.collection('hotels');
-
+  // Get hotelId from request params
   var hotelId = req.params.hotelId;
   console.log("GET hotelId", hotelId);
 
   // Retrieve result for given Id.
-  collection
-    .findOne({
-      _id : ObjectId(hotelId)
-    }, function(err, doc) {
-      res
-        .status(200)
-        .json(doc);
-    })
-
+  Hotel
+    .findById(hotelId)
+    .exec(function(err, doc) {
+      if (err) {
+        res
+          .status(400)
+          .send("No hotel found for ID " + hotelId);
+      } else {
+        res
+          .status(200)
+          .json(doc);
+      }
+    });
 };
 
+// Adds a single hotel to the db
 module.exports.addSingleHotel = function (req, res) {
   // Connect to database
   var db = dbconn.get();
